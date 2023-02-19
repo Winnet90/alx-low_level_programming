968d6a15d4a260b1672f4cca306747e2cdcf58f8
Winnet90 <winnetndlovu@gmail.com>
2023-02-19 08:39:44 -0800

    3 numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of three digits
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	int a, b, m;
+
+	for (a = 48; a < 58; a++)
+	{
+		for (b = 49; b < 58; b++)
+		{
+			for (m = 50; m < 58; m++)
+			{
+				if (m > b && b > a)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(m);
+					if (a != 55 || b != 56)
+					{
+						putchar(',');
+						putchar(' ');
+
+					}
+				}
+			}
+		}
+	}
+	putchar('\n')
+	return (0);
+}
