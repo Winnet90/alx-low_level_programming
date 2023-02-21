9329e5f0746e6a1fb79fafeafe29625d9a2b2c9e
Winnet90 <winnetndlovu@gmail.com>
2023-02-21 15:57:02 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+ /**
+  * function _abs - function that prints the absolute value of an int
+  * @n: int in question
+  *
+  * Return: abs int
+  */
+
+int _abs(int)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
