4a78df33501ea21da5c06d84d0b0bfe1e66552ed
Winnet90 <winnetndlovu@gmail.com>
2023-02-23 08:51:39 -0800

    task3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,41 +10,29 @@
  * Return: void
  */
 
-void print_remaining_days(int month, int day, int year);
-
-/**
- * print_remaining_days - prints the remaining days
- * @month: the current month
- * @day: the day
- * @year: the current year
- * Return: void
- */
-
 void print_remaining_days(int month, int day, int year)
-{
-	if ((year % 4 == 0) || (year % 100 == 0 && year % 400 == 0))
-	{
-		int leap_days = 366;
 
-		if (day >= 60 && month > 2)
-		{
-			day++;
-		}
+{
+	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
+	{
+	if (month > 2 && day >= 60)
+	{
+		day++;
+	}
 		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", leap_days - day);
+		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
-		if (day == 60 && month == 2)
+		if (month == 2 && day == 60)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
 		{
-			int standard_days = 365;
-
 			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", standard_days - day);
+			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
+
 }
