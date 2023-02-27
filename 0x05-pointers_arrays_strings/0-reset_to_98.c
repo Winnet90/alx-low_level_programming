b9531621be287864d768341f3ac7e09afe7a13b5
Winnet90 <winnetndlovu@gmail.com>
2023-02-27 06:05:03 -0800

    task1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - updates the value of a variable to 98 using pointers
+ *@n: integer to be updated
+ *
+ * Return: 0 Always
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
