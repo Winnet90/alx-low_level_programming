b71cda68b5615c1887b4a723ca71e9b2f684dd5b
Winnet90 <winnetndlovu@gmail.com>
2023-02-27 08:33:28 -0800

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: char to check
+ *
+ * Return: 0 Always
+ */
+
+int _strlen(char *s)
+{
+	int a = 0; /*start count from 0 */
+
+	for (; *s++;)
+		a++;
+	return (a);
+
+}
+
+
